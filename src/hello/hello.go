cfc07b3ee266c2501faeb6aa22896a95a1d2004a
Fernando de Lima <fernando.dpaula@gmail.com>
2019-09-23 22:32:14 -0300

    primero

---
+++ src/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Fernando"
+	var versao float32 = 1.1
+
+	fmt.Println("Olá senhor ", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+}
