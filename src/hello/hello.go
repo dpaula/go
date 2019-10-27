e21245aa7bbd835835a0b06aafab011f172fdc3a
Fernando de Lima <fernando.dpaula@gmail.com>
2019-10-26 22:06:25 -0300

    doc

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -7,13 +7,17 @@ import (
 
 func main() {
 
+	//não é necessário incluir o tipo da variável na declaração
 	var nome = "Fernando"
 	var versao float32 = 1.1
+
+	//declaração de variável sem o var
 	idade := 22
 
 	fmt.Println("Olá senhor ", nome, "sua idae é", idade)
 	fmt.Println("Este programa está na versão", versao)
 
+	//para descobrir o tipo da variável
 	fmt.Println("O tipo da variável é", reflect.TypeOf(nome))
 
 }
