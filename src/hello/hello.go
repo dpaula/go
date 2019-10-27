2c94af58a722da0950b726909c472a0826a5c76c
Fernando de Lima <fernando.dpaula@gmail.com>
2019-10-26 23:21:09 -0300

    organizando em funções

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,21 +6,11 @@ import (
 
 func main() {
 
-	//não é necessário incluir o tipo da variável na declaração
-	var nome = "Fernando"
-	var versao float32 = 1.1
+	exibeInformacoes()
 
-	fmt.Println("Olá senhor ", nome)
-	fmt.Println("Este programa está na versão", versao)
+	exibeMenu()
 
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("3 - Sair")
-
-	var comando int
-
-	//pegando valor no terminal e jogando na variável comando, usando & para apontar pra referencia em memória
-	fmt.Scan(&comando)
+	comando := leComando()
 
 	// no go, o switch não precisa de break
 	switch comando {
@@ -30,9 +20,35 @@ func main() {
 		fmt.Println("Exibindo logs...")
 	case 3:
 		fmt.Println("Saindo do programa")
+
 	default:
 		fmt.Println("Comando não existe")
 
 	}
+}
+
+func exibeInformacoes() {
+
+	//não é necessário incluir o tipo da variável na declaração
+	var nome = "Fernando"
+	var versao float32 = 1.1
+
+	fmt.Println("Olá senhor ", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Sair")
+}
+
+func leComando() int {
+
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+
+	return comandoLido
 
 }
